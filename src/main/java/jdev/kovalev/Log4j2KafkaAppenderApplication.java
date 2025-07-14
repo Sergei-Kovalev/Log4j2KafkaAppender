912279b858a4f8f6245191a354d1d557e3eec9fb
Sergey Kovalev <s.kovalev.jdev@gmail.com>
2025-07-14 15:24:32 +0300

    Initial commit

---
+++ src/main/java/jdev/kovalev/Log4j2KafkaAppenderApplication.java
@@ -0,0 +1,13 @@
+package jdev.kovalev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Log4j2KafkaAppenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Log4j2KafkaAppenderApplication.class, args);
+	}
+
+}
